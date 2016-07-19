7eb7c070c363365c68616184fcac5371f39f4f5b
Germano Nicolini <germanogmn1@gmail.com>
2016-07-19 15:49:59 -0300

    raise error instead of returning false

--- lib/vindi/charge.rb
+++ lib/vindi/charge.rb
@@ -2,9 +2,7 @@ module Vindi
   class Charge
     class Refund < Base
       def self.create(params = {})
-        charge_id = params[:charge_id]
-        return false unless charge_id
-
+        charge_id = params.fetch(:charge_id)
         resp = Request.new(:post, "charges/#{charge_id}/refund", params).perform
         parse(resp)
       end
